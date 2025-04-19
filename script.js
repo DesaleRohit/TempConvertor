b14dad0211fa59e151819d6681abadddd607edc6
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-19 09:25:20 +0530

    [MODIFIED]:Rohit - Added new function

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let inputCelsius = document.querySelector('#celsius');
 let inputFahrenheit = document.querySelector('#fahrenheit');
 let inputKelvin = document.querySelector('#kelvin');
 let btn = document.querySelector('#reset');
+
+let tempC;
+let tempF;
+let tempK;
+
+function celsiusToFahrenheitAndKelvin(c) {
+    tempF = (c * 9 / 5) + 32;
+    tempK = (c + 273.15);
+}
+
+
+function showCelsiusToOther() {
+    let showCelsius = parseFloat(inputCelsius.value);
+    celsiusToFahrenheitAndKelvin(showCelsius);
+    document.querySelector('#fahrenheit').value = tempF;
+    document.querySelector('#kelvin').value = tempK;
+}
+
+
