695abd804198bae1df5476b4d8a21250beaca3d8
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-19 09:00:39 +0530

    [ADDED]:Rohit - Temp convertor structure

---
+++ script.js
@@ -0,0 +1,4 @@
+let inputCelsius = document.querySelector('#celsius');
+let inputFahrenheit = document.querySelector('#fahrenheit');
+let inputKelvin = document.querySelector('#kelvin');
+let btn = document.querySelector('#reset');
