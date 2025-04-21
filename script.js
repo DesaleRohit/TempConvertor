4b09ca966199fce545b3dbd8a2ab3f2fec2eb197
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-21 09:45:15 +0530

    [MODIFIED]:Rohit - button reset feature

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ let inputCelsius = document.querySelector('#celsius');
 let inputFahrenheit = document.querySelector('#fahrenheit');
 let inputKelvin = document.querySelector('#kelvin');
 let btn = document.querySelector('#reset');
+
+let tempC;
+let tempF;
+let tempK;
+
+function celsiusToFahrenheitAndKelvin(c) {
+    tempF = (c * 9 / 5) + 32;
+    tempK = (c + 273.15);
+}
+
+function fahrenheitToCelsiusAndKelvin(f) {
+    tempC = (f - 32) * 5 / 9;
+    tempK = (f - 32) * 5 / 9 + 273.15;
+}
+
+
+function showCelsiusToOther() {
+    let showCelsius = parseFloat(inputCelsius.value);
+    celsiusToFahrenheitAndKelvin(showCelsius);
+    document.querySelector('#fahrenheit').value = tempF;
+    document.querySelector('#kelvin').value = tempK;
+}
+
+
+function showFahrenheitToOther() {
+    let showFahrenheit = parseFloat(inputFahrenheit.value);
+    fahrenheitToCelsiusAndKelvin(showFahrenheit);
+    document.querySelector('#celsius').value = tempC;
+    document.querySelector('#kelvin').value = tempK;
+}
+
+btn.addEventListener('click', () => {
+    inputCelsius.value = '';
+    inputFahrenheit.value = '';
+    inputKelvin.value = '';
+});
+
